30d1b22d79d0f18b9827fbbf8f5f8e67df6d334c
Alucardevil <alucardevil1973@gmail.com>
2023-11-20 09:00:51 -0500

    first update

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('quote-form');
+const thankYouPopup = document.getElementById('thank-you');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    thankYouPopup.style.display = 'block';
+    setTimeout(() => {
+        thankYouPopup.style.display = 'none';
+    }, 3000);
+});
